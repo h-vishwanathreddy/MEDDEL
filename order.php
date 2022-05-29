6c164b2c53cee54867726585e9c656a669ea51fe
Srujan-Rao <srujanrao4@gmail.com>
2022-05-30 01:38:44 +0530

    Init order

--- order.php
+++ order.php
@@ -45,7 +45,7 @@
 
         <form action="" method="POST" class="order">
             <fieldset>
-                <legend>Selected Food</legend>
+                <legend>Selected Medicines</legend>
 
                 <div class="food-menu-img">
 
